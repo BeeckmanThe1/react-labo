c004fc3cc2f7972b8365b65c4c2d20bc8ca33fae
thomas.opdebeeck <thomas.opdebeeck@adflumine.com>
2023-05-28 15:40:12 +0200

    clean up

--- src/hybrid/components/pages/basics/hooks/use-state/demos/demo1.page.tsx
+++ src/hybrid/components/pages/basics/hooks/use-state/demos/demo1.page.tsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react'
 import {meta} from './demo1.page.meta';
-import {Button, Row, Space} from 'antd';
+import {Button, Space, message} from 'antd';
 import AppShell from '../../../../../app-shell/app.shell';
 import CodeSnippet, {CODE_LANGUAGES} from '../../../../../partials/code-snippet/CodeSnippet';
 
+const isRenderingInBrowser = typeof window !== 'undefined'
 const Demo = () => {
     const [count1, setCount1] = useState(0)
     const [count2, setCount2] = useState(0)
 
     // check is needed because of SSR + hydration architecture
-    if (typeof window !== 'undefined') window.alert('RENDER')
+    if (isRenderingInBrowser) message.info('RENDERING', 0.4)
 
     const bumpCount1 = () => setCount1(prev => prev + 1)
     const bumpCount2 = () => setCount2(prev => prev + 1)
@@ -25,37 +26,35 @@ const Demo = () => {
         </Space>
     </Space>
 }
+const demoAsString = `
+const Demo = () => {
+    const [count1, setCount1] = useState(0)
+    const [count2, setCount2] = useState(0)
 
-const UseStateDemoPage = () => {
-    const children = `
-    const Demo = () => {
-        const [count1, setCount1] = useState(0)
-        const [count2, setCount2] = useState(0)
-    
-        // needed because SSR + hydration archetecture
-        if (typeof window !== 'undefined') window.alert('RENDER')
-    
-        const bumpCount1 = () => setCount1(prev => prev + 1)
-        const bumpCount2 = () => setCount2(prev => prev + 1)
-    
-        return <Space direction={'vertical'}>
-            <Space>
-                <Button onClick={bumpCount1}>Click me</Button>
-                <b>Count 1: {count1}</b>
-            </Space>
-            <Space>
-                <Button onClick={bumpCount2}>Click me</Button>
-                <b>Count 2: {count2}</b>
-            </Space>
+    message.info('RENDERING', 0.4)
+
+    const bumpCount1 = () => setCount1(prev => prev + 1)
+    const bumpCount2 = () => setCount2(prev => prev + 1)
+
+    return <Space direction={'vertical'}>
+        <Space>
+            <Button onClick={bumpCount1}>Click me</Button>
+            <b>Count 1: {count1}</b>
         </Space>
-    }
+        <Space>
+            <Button onClick={bumpCount2}>Click me</Button>
+            <b>Count 2: {count2}</b>
+        </Space>
+    </Space>
+}
         `
+const UseStateDemoPage = () => {
     return <AppShell>
         <h1>{UseStateDemoPage.meta.title}</h1>
         <Demo/>
 
         <CodeSnippet language={CODE_LANGUAGES.JS}>
-            {children}
+            {demoAsString}
         </CodeSnippet>
     </AppShell>
 }
