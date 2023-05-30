700760af28a939eea30fc540945de4dcea7f2af3
thomas.opdebeeck <thomas.opdebeeck@adflumine.com>
2023-05-30 12:48:08 +0200

    improve linting

---
+++ src/hybrid/components/pages/basics/hooks/use-reducer/use-reducer.page.meta.ts
@@ -0,0 +1,9 @@
+import { PageMeta } from '../../../page.model';
+
+export const meta: PageMeta = {
+    title: 'useReducer',
+    metaDescription: 'Info about useReducer',
+    slug: 'use-reducer',
+    url: '/basics/hooks/use-reducer',
+    order: 2
+}
--- src/hybrid/components/pages/homepage/homepage.page.tsx
+++ src/hybrid/components/pages/homepage/homepage.page.tsx
@@ -6,7 +6,9 @@ import AppShell from '../../app-shell/app.shell';
 const Homepage: React.FC & { meta: PageMeta } = () => {
     return (
         <AppShell>
-            <div className={'hompage'}/>
+            <div className={'homepage'}>
+                This is my react labo where I will document basic concepts and play around with different ways to build, manage state, test etc.
+            </div>
         </AppShell>
     );
 }
